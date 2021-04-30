ffd8e43ccb6a0e4470ab8b1df45965617a1bd457
pythonFCG <5120009+pythonFCGa@user.noreply.gitee.com>
2021-04-30 09:27:57 +0800

    syc

--- src/main/java/com/zzspace/controller/MainController.java
+++ src/main/java/com/zzspace/controller/MainController.java
@@ -6,9 +6,16 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.Clipboard;
-import javafx.scene.text.Text;
+import org.apdplat.word.WordSegmenter;
+import org.apdplat.word.segmentation.Word;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainController {
+    private static final String POLICY_PATTERN = "82\\d{16}";
+    private static final String CHANEEL_POLICY_PATTERN = "2\\d{19}";
     @FXML
     private TextArea textAreaInput;
     @FXML
@@ -39,5 +46,16 @@ public class MainController {
         if (input == null || input.equals(""))
             return;
         textAreaInput.setText(input);
+        List<Word> seg = WordSegmenter.seg(input);
+        System.out.println(seg);
+        Matcher matcher = Pattern.compile(POLICY_PATTERN).matcher(input);
+        if (matcher.find()) {
+            textPolicy.setText(matcher.group());
+        }
+        matcher = Pattern.compile(CHANEEL_POLICY_PATTERN).matcher(input);
+        if (matcher.find()) {
+            textChennelPolicy.setText(matcher.group());
+        }
+
     }
 }
